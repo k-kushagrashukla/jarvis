cb0234d09d4aa98038cccceba16bbbbf7bd38dec
k-kushagrashukla <kushagra.shukla396@gmail.com>
2024-09-13 23:33:01 +0530

    first commit

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "illuminati": "https://youtu.be/tOM-nWPcR4U?si=ilO72ExIKEGjXAAv",
+    "bad": "https://youtu.be/EAJCr5vvYz0?si=ahqGDt8rLRvIXcNb",
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
+    "tere":"https://youtu.be/94vimg-so20?si=jLp7Y9ZtyNS37XMQ"
+}
